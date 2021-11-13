41f8ca1a8d3658feeb90a2b52892d10206114daa
Chris Allen <chris.allen@freshconsulting.com>
2021-11-13 12:28:56 -0800

    add profile portion of intro

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import Logo from "../images/logo.svg";
 
 const Header = ({ siteTitle }) => (
   <header className="header">
@@ -9,26 +9,19 @@ const Header = ({ siteTitle }) => (
       <div className="header__wrapper">
         <div className="header__logo">
           <Link to="/" className="header__logo-link">
-            <StaticImage
-              src="../images/logo.svg"
-              width={77}
-              quality={100}
-              placeholder="none"
-              formats={["auto", "webp", "avif"]}
-              alt="Chris Allen's Logo"
-            />
-              <span className="sr-only">{siteTitle}</span>
-              <span className="header__logo-name">
-                <span>h</span>
-                <span>r</span>
-                <span>i</span>
-                <span>s</span>
-                <span> A</span>
-                <span>l</span>
-                <span>l</span>
-                <span>e</span>
-                <span>n</span>
-              </span>
+            <Logo />
+            <span className="sr-only">{siteTitle}</span>
+            <span className="header__logo-name">
+              <span>h</span>
+              <span>r</span>
+              <span>i</span>
+              <span>s</span>
+              <span> A</span>
+              <span>l</span>
+              <span>l</span>
+              <span>e</span>
+              <span>n</span>
+            </span>
           </Link>
         </div>
         <nav className="header__nav">
--- src/components/intro.js
+++ src/components/intro.js
@@ -1,12 +1,34 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Intro = () => (
   <section className="intro">
     <div className="container">
-      <h1 className="intro__title">Hi, I’m Chris Allen</h1>
-      <h2 className="intro__sub-title">Front-end developer</h2>
-      <p className="intro__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer risus est, bibendum eget leo at, interdum pharetra felis. Etiam sed est in nibh bibendum faucibus a id justo. Vestibulum sit amet diam.</p>
-      <a href="#" className="button intro__button">See Work</a>
+      <div className="intro__wrapper">
+        <h1 className="intro__title">Hi, I’m Chris Allen</h1>
+        <h2 className="intro__sub-title">Front-end developer</h2>
+        <p className="intro__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer risus est, bibendum eget leo at, interdum pharetra felis. Etiam sed est in nibh bibendum faucibus a id justo. Vestibulum sit amet diam.</p>
+        <a href="/foo" className="button intro__button">See Work</a>
+        <div className="intro__profile-wrapper">
+          <StaticImage
+            src="../images/profile.png"
+            width={387}
+            quality={100}
+            placeholder="none"
+            formats={["auto", "webp", "avif"]}
+            alt="Chris Allen's Logo"
+          />
+          <span class="intro__op intro__op--foreground intro__op--one">[ ]</span>
+          <span class="intro__op intro__op--foreground intro__op--two">++</span>
+          <span class="intro__op intro__op--foreground intro__op--three">{ "<>" }</span>
+          <span class="intro__op intro__op--foreground intro__op--four">{ "{ }" }</span>
+          <span class="intro__op intro__op--background intro__op--five">{ "!==" }</span>
+          <span class="intro__op intro__op--background intro__op--six">{ "!==" }</span>
+          <span class="intro__op intro__op--background intro__op--seven">{ "!==" }</span>
+          <span class="intro__op intro__op--background intro__op--eight">{ "!==" }</span>
+          <span class="intro__op intro__op--background intro__op--nine">{ "!==" }</span>
+        </div>
+      </div>
     </div>
   </section>
 )
