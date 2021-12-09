e859c88f08275a7b95eb0de98ff1863de8549e2e
Chris Allen <chris.allen@freshconsulting.com>
2021-12-08 21:22:25 -0800

    add pagination / refactor posts data

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,22 +5,64 @@ const path = require(`path`)
    return graphql(`
      {
        allWpPost(sort: { fields: [date], order:DESC }) {
-         nodes {
-           title
-           excerpt
-           content
-           slug
+        edges {
+         node {
+            slug
+            title
+            featuredImage {
+              node {
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(
+                      avifOptions: {quality: 10, lossless: false}
+                      layout: FIXED
+                      webpOptions: {quality: 100}
+                      width: 1920
+                      height: 650
+                    )
+                  }
+                }
+              }
+            }
+            posts {
+              description
+              siteLink
+              client
+              role
+              team
+              images {
+                projectImage {
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData(layout: FIXED, width: 1100)
+                    }
+                  }
+                  sourceUrl
+                }
+              }
+            }
+          }
          }
        }
      }
    `).then(result => {
-    result.data.allWpPost.nodes.forEach(node => {
+    const posts = result.data.allWpPost.edges
+    posts.forEach((node, index) => {
         createPage({
-          path: node.slug,
+          path: node.node.slug,
           component: path.resolve(`./src/templates/post.js`),
           context: {
-            // This is the $slug variable passed to blog-post.js
-            slug: node.slug,
+            title: node.node.title,
+            featured: node.node.featuredImage,
+            description: node.node.posts.description,
+            siteLink: node.node.posts.siteLink,
+            client: node.node.posts.client,
+            role: node.node.posts.role,
+            team: node.node.posts.team,
+            images: node.node.posts.images,
+            slug: node.node.slug,
+            prev: index === 0 ? null : posts[index - 1].node,
+            next: index === posts.length - 1 ? null : posts[index + 1].node
           },
         })
       })
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,15 +1,16 @@
 import * as React from "react"
 import Layout from "../components/layout"
-import { graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
 import Icon from "../images/svgs/arrow-right.svg";
 import Seo from "../components/seo"
 
-const PostTemplate = ( { data } ) => {
-  const post = data.allWpPost.nodes[0];
+const PostTemplate = ( { pageContext } ) => {
+  const nextLink = pageContext.next !== null ? '/' + pageContext.next.slug : '';
+  const prevLink = pageContext.prev !== null ?  '/' + pageContext.prev.slug : '';
+  const paginationClass = pageContext.prev === null ? 'single-post__pagination single-post__pagination--end' : 'single-post__pagination'
 
-  const imageGallery = post.posts.images.map(( images ) =>
+  const imageGallery = pageContext.images.map(( images ) =>
     <div key={ images.projectImage.sourceUrl } className="single-post__image-wrapper" data-sal="slide-up" data-sal-duration="900">
       { images.projectImage.localFile.childImageSharp !== null ?
         <GatsbyImage
@@ -18,7 +19,7 @@ const PostTemplate = ( { data } ) => {
           quality={100}
           placeholder="none"
           formats={["auto", "webp", "avif", "gif"]}
-          alt={ post.title }
+          alt={ pageContext.title }
           className="single-post__image"
         />
         :
@@ -29,46 +30,58 @@ const PostTemplate = ( { data } ) => {
 
   return(
     <Layout>
-      <Seo title={ post.title } />
+      <Seo title={ pageContext.title } />
       <div className="single-post">
         <div className="single-post__hero-wrapper">
           <div data-sal="fade" data-sal-easing="ease" data-sal-duration="900">
             <GatsbyImage
-              image={ post.featuredImage.node.localFile.childImageSharp.gatsbyImageData }
+              image={ pageContext.featured.node.localFile.childImageSharp.gatsbyImageData }
               width={1920}
               quality={100}
               placeholder="none"
               formats={["auto", "webp", "avif"]}
-              alt={ post.title }
-              className=""
+              alt={ pageContext.title }
+              className="single-post__hero"
             />
           </div>
         </div>
         <div className="container">
-          <h1 className="single-post__section-title" data-sal="slide-up" data-sal-duration="900">{ post.title }</h1>
+          <h1 className="single-post__section-title" data-sal="slide-up" data-sal-duration="900">{ pageContext.title }</h1>
           <div className="single-post__intro" data-sal="slide-up" data-sal-duration="900">
             <div className="single-post__left-col">
-              <div className="single-post__intro-description" dangerouslySetInnerHTML={{__html: post.posts.description}} />
-              <a href={ post.posts.siteLink } target="_blank" rel="noreferrer" className="button button--ghost-dark">
+              <div className="single-post__intro-description" dangerouslySetInnerHTML={{__html: pageContext.description}} />
+              <a href={ pageContext.siteLink } target="_blank" rel="noreferrer" className="button button--ghost-dark">
                 <span className="button__text">Visit Site</span>
                 <span className="button__icon button__icon--right"><Icon /></span>
               </a>
             </div>
             <div className="single-post__right-col">
               <h2 className="single-post__sidebar-heading">Client</h2>
-              <p className="single-post__sidebar-description">{ post.posts.client }</p>
+              <p className="single-post__sidebar-description">{ pageContext.client }</p>
               <h2 className="single-post__sidebar-heading">Role</h2>
-              <p className="single-post__sidebar-description">{ post.posts.role }</p>
+              <p className="single-post__sidebar-description">{ pageContext.role }</p>
               <h2 className="single-post__sidebar-heading">Team</h2>
-              <p className="single-post__sidebar-description">{ post.posts.team }</p>
+              <p className="single-post__sidebar-description">{ pageContext.team }</p>
             </div>
           </div>
           { imageGallery }
-          <div className="single-post__more">
-            <Link to="/#work" className="button button--ghost-dark">
-              <span className="button__text">View All Work</span>
-              <span className="button__icon button__icon--right"><Icon /></span>
-            </Link>
+          <div className={paginationClass} data-sal="fade" data-sal-duration="900">
+            { prevLink ?
+              <Link to={prevLink} className="single-post__prev">
+                <span className="single-post__prev-icon"><Icon /></span>
+                { pageContext.prev.title }
+              </Link>
+              :
+              ""
+            }
+            { nextLink ?
+              <Link to={nextLink} className="single-post__next">
+                { pageContext.next.title }
+                <span className="single-post__next-icon"><Icon /></span>
+              </Link>
+              :
+              ""
+            }
           </div>
         </div>
       </div>
@@ -77,46 +90,3 @@ const PostTemplate = ( { data } ) => {
 }
 
 export default PostTemplate
-
-export const query = graphql`
-  query($slug: String!) {
-    allWpPost(filter: { slug: { eq: $slug } }) {
-      nodes {
-        title
-        content
-        featuredImage {
-          node {
-            localFile {
-              childImageSharp {
-                gatsbyImageData(
-                  avifOptions: {quality: 10, lossless: false}
-                  layout: FIXED
-                  webpOptions: {quality: 100}
-                  width: 1920
-                  height: 650
-                )
-              }
-            }
-          }
-        }
-        posts {
-          description
-          siteLink
-          client
-          role
-          team
-          images {
-            projectImage {
-              localFile {
-                childImageSharp {
-                  gatsbyImageData(layout: FIXED, width: 1100)
-                }
-              }
-              sourceUrl
-            }
-          }
-        }
-      }
-    }
-  }
-`
