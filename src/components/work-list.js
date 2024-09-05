ecfc398748b198607ae3a66c0a7cd1e4db655d95
Angela Allen <angelaallen@Angelas-Mini.domain>
2024-09-04 18:35:35 -0700

    accessibility updates

--- src/components/work-list.js
+++ src/components/work-list.js
@@ -5,29 +5,29 @@ import { Link } from "gatsby"
 import Icon from "../images/svgs/arrow-right.svg";
 
 
-const Work = ( props ) => {
+const Work = (props) => {
   const work = props.data;
 
-  const workList = work.map(( work ) =>
-    <div key={ work.title } className="work-list__item">
+  const workList = work.map((work) =>
+    <div key={work.title} className="work-list__item">
       <div data-sal="slide-up" data-sal-easing="ease" data-sal-duration="900">
-      <Link to={`/work${work.uri}`}>
-        <GatsbyImage
-          image={ work.posts.homeImage.localFile.childImageSharp.gatsbyImageData}
-          objectFit="contain"
-          width={100}
-          quality={100}
-          placeholder="none"
-          formats={["auto", "webp", "avif"]}
-          alt={work.title}
-          className="work-list__item-image"
-        />
-      </Link>
+        <Link to={`/work${work.uri}`} aria-label={work.title}>
+          <GatsbyImage
+            image={work.posts.homeImage.localFile.childImageSharp.gatsbyImageData}
+            objectFit="contain"
+            width={100}
+            quality={100}
+            placeholder="none"
+            formats={["auto", "webp", "avif"]}
+            alt={work.title}
+            className="work-list__item-image"
+          />
+        </Link>
       </div>
       <div className="work-list__item-content-wrapper" data-sal="slide-up" data-sal-easing="ease" data-sal-duration="900" data-sal-delay="200">
         <div className="work-list__item-right-col">
           <h3 className="work-list__item-title">{work.title}</h3>
-          <div className="work-list__item-description" dangerouslySetInnerHTML={{__html: work.content}} />
+          <div className="work-list__item-description" dangerouslySetInnerHTML={{ __html: work.content }} />
         </div>
         <div className="work-list__item-left-col">
           <Link to={`/work${work.uri}`} className="button button--ghost-dark">
@@ -43,7 +43,7 @@ const Work = ( props ) => {
     <section id="work" className="work-list">
       <div className="container">
         <h2 className="work-list__section-title" data-sal="slide-up" data-sal-easing="ease" data-sal-duration="900">Selected Work</h2>
-        { workList }
+        {workList}
       </div>
     </section>
   )
